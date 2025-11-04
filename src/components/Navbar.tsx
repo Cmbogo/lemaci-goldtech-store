83a312d66a513940a1d2db07c3aeaf07d6c8d5b9
Cicily Mbogo <145114713+Cmbogo@users.noreply.github.com>
2025-11-04 09:35:53 +0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ export const Navbar = () => {
         <div className="flex items-center justify-between">
           {/* Logo */}
           <div className="flex items-center">
-            <img src={lemaciy Logo} alt="Lemaciy Logo" className="w-12 h-12 object-contain" />
+            <img src={lemaci-Logo} alt="Lemaci Logo" className="w-12 h-12 object-contain" />
           </div>
 
           {/* Desktop Navigation */}
